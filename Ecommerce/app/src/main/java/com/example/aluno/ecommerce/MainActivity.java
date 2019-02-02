b3459f5be1b482c79ce261cbaf6bc790eab9c92c
axelzito <axel.ataides@gmail.com>
2019-02-02 15:51:57 -0200

    ajustes tela

--- Ecommerce/app/src/main/java/com/example/aluno/ecommerce/MainActivity.java
+++ Ecommerce/app/src/main/java/com/example/aluno/ecommerce/MainActivity.java
@@ -29,48 +29,25 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("TAG", "usuário clicou no item: " + lista.getItemAtPosition(position).toString());
                 Toast.makeText(getApplicationContext(),"Você clicou em: "+ equipes.get(position).toString(),Toast.LENGTH_LONG).show();
-                mostraTela(equipes.get(position).toString());
+                mostraTela(equipes.get(position).getNome(), equipes.get(position).getSubnome(), equipes.get(position).getImagem());
             }
         });
 
     }
 
-    private void mostraTela(String equipe) {
+    private void mostraTela(String equipe, String subtext, int imagem) {
         Intent it = new Intent(this, Produto.class);
         it.putExtra("equipe", equipe);
+        it.putExtra("subtext", subtext);
+        it.putExtra("imagem", imagem);
         startActivity(it);
     }
-
-
-    /*
-
-     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final ListView lista = (ListView) findViewById(R.id.listEquipe);
-        final ArrayList<String> equipes = preecheDados();
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, equipes);
-        lista.setAdapter(arrayAdapter);
-
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("TAG", "usuário clicou no item: " + lista.getItemAtPosition(position).toString());
-                Toast.makeText(getApplicationContext(),"Você clicou em: "+ equipes.get(position).toString(),Toast.LENGTH_LONG).show();
-                mostraTela(equipes.get(position).toString());
-            }
-        });
-
-    }
-    * */
 
     private ArrayList<Times> preencheDados() {
 
         ArrayList<Times> dados = new ArrayList<Times>();
 
-        Times t = new Times("Iphone 8s", "tela 5' 8 de ram processador topzera etc e tal", 5000, R.drawable.atleti);
+        Times t = new Times("Iphone 8s 100% original dos bons pá tal topzera", "tela 5' 8 de ram processador topzera etc e tal", 5000, R.drawable.atleti);
         dados.add(t);
 
        /* t = new Times("Banfield",R.drawable.banfield);
--- Ecommerce/app/src/main/java/com/example/aluno/ecommerce/Produto.java
+++ Ecommerce/app/src/main/java/com/example/aluno/ecommerce/Produto.java
@@ -3,6 +3,7 @@ package com.example.aluno.ecommerce;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Produto extends AppCompatActivity {
@@ -14,8 +15,16 @@ public class Produto extends AppCompatActivity {
 
         Intent intent = getIntent();
         String equipe = intent.getStringExtra("equipe");
+        String subtext = intent.getStringExtra("subtext");
+        //int imagem = Integer.parseInt(intent.getStringExtra("imagem"));
 
         TextView textView = (TextView) findViewById(R.id.produto_texto);
         textView.append(equipe + "");
+
+        //TextView textView2 = (TextView) findViewById(R.id.produto_texto);
+        //textView.append(equipe + "");
+
+        //ImageView imgView = (ImageView) findViewById(R.id.imagem_produto);
+        //imgView.setImageResource(imagem);
     }
 }
