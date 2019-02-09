d40b0370f5d3c48f2016c051d597c102aee2effc
axelzito <axel.ataides@gmail.com>
2019-02-09 11:54:57 -0200

    adicionando novas telas

---
+++ AplicacaoBD/app/src/main/java/com/example/aluno/aplicacaobd/AdicionarLivro.java
@@ -0,0 +1,40 @@
+package com.example.aluno.aplicacaobd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AdicionarLivro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_livro);
+
+        Button btnCadastro = (Button) findViewById(R.id.botao_adicionar_dados);
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Controller crud = new Controller(getBaseContext());
+
+             EditText titulo = (EditText) findViewById(R.id.nome_livro);
+             EditText autor = (EditText) findViewById(R.id.nome_autor);
+             EditText editora = (EditText) findViewById(R.id.nome_editora);
+
+             String tituloString = titulo.getText().toString();
+             String autorString = autor.getText().toString();
+             String editoraString = editora.getText().toString();
+
+             String resultado;
+
+             resultado = crud.insereDados(tituloString, autorString, editoraString);
+             Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
+         }
+        });
+    }
+
+}
--- AplicacaoBD/app/src/main/java/com/example/aluno/aplicacaobd/MainActivity.java
+++ AplicacaoBD/app/src/main/java/com/example/aluno/aplicacaobd/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.aluno.aplicacaobd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void addlivro() {
+        /* intent = new Intent(this, Logado.class);
+        TextView email = (TextView) findViewById(R.id.email);
+        String login = "Olá, " + email.getText().toString();
+        intent.putExtra("bla", login);
+        startActivity(intent);*/
+
+        //intent = new Intent();
     }
 }
